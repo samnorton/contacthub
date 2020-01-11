d106e4cf6d987b0d172a03ea0f971c9ecfe257b0
Sam Norton <samuelnico.norton@gmail.com>
2020-01-11 19:49:40 +0800

    Added validation code and class for the add contact form.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,6 @@ class Contact < ApplicationRecord
   validates_length_of :name, minimum: 3
   validates_length_of :mobile, minimum: 7, maximum: 15, allow_blank: true
   validates_length_of :phone, minimum: 7, maximum: 15, allow_blank: true
-  validates_length_of :zip, minimum: 4, maximum: 12, allow_blank: true
 
 
 end
