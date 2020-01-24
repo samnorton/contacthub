a94ae124845f3d5194bddb362439408b7fb44b81
Sam Norton <samuelnico.norton@gmail.com>
2020-01-24 19:35:22 +0800

    Finished edit profile feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'homepage#index'
+  
   post '/categories', to: 'categories#create'
 
   scope '/dashboard' do
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
     end
   end 
   
-  root 'homepage#index'
+  
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
