b636429f7f1537e70bbc2691be846d0280c0b431
Sam Norton <samuelnico.norton@gmail.com>
2020-06-02 12:33:39 +0800

    Add files to heroku.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Contacthub
       html_tag
     }
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
